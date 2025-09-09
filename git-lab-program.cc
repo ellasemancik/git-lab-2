cd0f0a2390d0ac46dead0f9f3a18bda88f19f781
Ella Semancik <es175224@ohio.edu>
2025-09-09 16:53:40 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ella Semancik
+ *        Date: September 3rd, 2025
+ * Description: Creating a C++ program in git-lab-2
+ repository and updating the online main branch
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
